308e2ad427a534a10c1a0ec33b25a40a54295205
HarryHandoko <harrydoko30@gmail.com>
2025-08-11 09:16:00 +0700

    feature/update

---
+++ database/migrations/1754878352633_tbl_web_profiles_schema.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class TblWebProfilesSchema extends Schema {
+  up () {
+    this.table('tbl_web_profiles', (table) => {
+      table.longText('moto').nullable();
+    })
+  }
+
+  down () {
+    this.table('tbl_web_profiles', (table) => {
+      // reverse alternations
+    })
+  }
+}
+
+module.exports = TblWebProfilesSchema
